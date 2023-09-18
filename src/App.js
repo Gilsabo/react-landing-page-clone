642945ed63f3ebecb339c38732fbb602929160d7
Gilsabo <gilsala_88@hotmail.com>
2023-09-18 16:39:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <div>
+            <a>
+              <img src="../src/assets/images/useform.png" alt="Useform-Logo" />
+            </a>
+            <ul>
+              <li>
+                <a>Pricing</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+              <li>
+                <a>How we're different</a>
+              </li>
+            </ul>
+            <div>
+              <a>Login</a>
+              <a>Start for free</a>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
