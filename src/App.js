66622764bedc7a4057cfbef162df71ffd456b245
Gilsabo <gilsala_88@hotmail.com>
2023-09-18 21:20:42 +0200

    Style hero section before the star section

--- src/App.js
+++ src/App.js
@@ -1,32 +1,67 @@
+import Background from '../src/assets/images/grid-overlay.png';
 import Useform from '../src/assets/images/useform.png';
 import styles from './App.module.scss';
 
 export default function App() {
   return (
     <div className="App">
-      <section>
-        <header>
-          <nav>
-            <a>
-              <img src={Useform} alt="Useform-Logo" />
-            </a>
-            <ul>
-              <li>
-                <a>Pricing</a>
-              </li>
-              <li>
-                <a>Blog</a>
-              </li>
-              <li>
-                <a>How we're different</a>
-              </li>
-            </ul>
+      <header>
+        <nav>
+          <a href="/">
+            <img
+              className={styles.logoHeader}
+              src={Useform}
+              alt="Useform-Logo"
+            />
+          </a>
+          <ul className={styles.unorderedListHeader}>
+            <li>
+              <a href="/#">Pricing</a>
+            </li>
+            <li>
+              <a href="/#">Blog</a>
+            </li>
+            <li>
+              <a href="/#">How we're different</a>
+            </li>
+          </ul>
+          <div className={styles.logInSectionHeader}>
+            <a href="/#">Login</a>
+            <a href="/#">Start for free</a>
+          </div>
+        </nav>
+      </header>
+      <section className={styles.sectionHero}>
+        <img src={Background} className={styles.imageHero} alt="background" />
+        <div className={styles.boxHero}>
+          <div className={styles.spanSectionHero}>
+            No-code forms, without the pain.
+          </div>
+          <h1 className={styles.headerSection}>
+            The easiest way to <br /> create an
+            <span> online form</span>
+          </h1>
+          <p>
+            Build forms in <b>minutes</b> instead of hours. <br />
+            The last form-builder you'll ever need.
+          </p>
+          <div>
+            <a>Create my first form for free</a>
+            <p>
+              Free plan, no credit-card -<a>View pricing</a>
+            </p>
             <div>
-              <a>Login</a>
-              <a>Start for free</a>
+              <p>Trusted by agencies & freelancers</p>
+              <div>
+                <p>*</p>
+                <p>*</p>
+                <p>*</p>
+                <p>*</p>
+                <p>*</p>
+              </div>
             </div>
-          </nav>
-        </header>
+          </div>
+        </div>
       </section>
     </div>
   );
